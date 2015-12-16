e60edf1f548171b6b3d9218af2bcd29f339dd35d
Jean-Michel TEXIER <jm.texier@gmail.com>
2015-12-16 18:58:19 +0100

    Split tests to parallelize implementation

--- test/200-process-order.js
+++ test/200-process-order.js
@@ -2,6 +2,7 @@ var expect = require('chai').expect;
 var process = require('../lib/process');
 
 describe('process.order(payload, callback)', function () {
+  
   it('calls back with an empty object', function (done) {
       process.order({}, function (err, result) {
       if (err) return done(err);
@@ -10,79 +11,22 @@ describe('process.order(payload, callback)', function () {
     });
   });
 
- it('must not process non STANDARD rate', function (done) {
-      var body = { prices: [ 91.73 ],
-                   quantities: [ 2 ],
-                   names:
-                    [ 'Tea',
-                      'Glove',
-                      'Worms',
-                      'Water',
-                      'Carrot',
-                      'Jacket',
-                      'Stinking cheese',
-                      'Net' ],
-                   country: 'HU',
-                   reduction: 'STRANGE' };
+  it('compute a simple request', function (done) {
+    var body = { prices: [ 91.73 ],
+                 quantities: [ 2 ],
+                 names:
+                  [ 'Tea' ],
+                 country: 'HU',
+                 reduction: 'STANDARD' };
 
-      process.order(body, function (err, result) {
-        if (err) return done(err);
-        expect(result).to.deep.equal({  });
-        done();
-      });
+    process.order(body, function (err, result) {
+      if (err) return done(err);
+      console.log("result:");
+      console.log(result);
+      expect(result.total).to.equal(232.9942);
+      done();
+    });
   });
 
- it('must check if prices / quantities / names are defined', function (done) {
-      var body = { prices: [ 91.73 ],
-                   quantities: [ 2, 3 ],
-                   country: 'HU',
-                   reduction: 'STANDARD' };
-
-      process.order(body, function (err, result) {
-        if (err) return done(err);
-        expect(result).to.deep.equal({  });
-        done();
-      });
-  });
-
-
- it('must check if prices / quantities / names have the same size', function (done) {
-      var body = { prices: [ 91.73 ],
-                   quantities: [ 2, 3 ],
-                   names:
-                    [ 'Tea',
-                      'Glove',
-                      'Worms',
-                      'Water',
-                      'Carrot',
-                      'Jacket',
-                      'Stinking cheese',
-                      'Net' ],
-                   country: 'HU',
-                   reduction: 'STANDARD' };
-
-      process.order(body, function (err, result) {
-        if (err) return done(err);
-        expect(result).to.deep.equal({  });
-        done();
-      });
-  });
-
-   it('compute a simple request', function (done) {
-      var body = { prices: [ 91.73 ],
-                   quantities: [ 2 ],
-                   names:
-                    [ 'Tea' ],
-                   country: 'HU',
-                   reduction: 'STANDARD' };
-
-      process.order(body, function (err, result) {
-        if (err) return done(err);
-        console.log("result:");
-        console.log(result);
-        expect(result.total).to.equal(232.9942);
-        done();
-      });
-   });
 });
 

dccf2f97a7558b07ad6c356fa15202e9a8da7c9a
Raimon Rafols <raimon.rafols@axa-groupsolutions.com>
2015-12-16 18:13:56 +0100

    adding tests for negative prices and negative quantities

--- test/200-process-order.js
+++ test/200-process-order.js
@@ -84,5 +84,37 @@ describe('process.order(payload, callback)', function () {
         done();
       })
    })
-})
 
+   it('check if prices are negative', function (done) {
+      var body = { prices: [ 91.73, -10.0 ],
+                   quantities: [ 2, 3 ],
+                   names:
+                    [ 'Tea',
+                      'Glove'],
+                   country: 'HU',
+                   reduction: 'STANDARD' };
+
+      process.order(body, function (err, result) {
+        if (err) return done(err);
+        expect(result).to.deep.equal({  });
+        done();
+      })
+    })
+
+    it('check if quantities are negative', function (done) {
+       var body = { prices: [ 91.73, 10.0, 2.38 ],
+                    quantities: [ 2, 3, -1 ],
+                    names:
+                     [ 'Tea',
+                       'Glove',
+                       'Worms'],
+                    country: 'HU',
+                    reduction: 'STANDARD' };
+
+       process.order(body, function (err, result) {
+         if (err) return done(err);
+         expect(result).to.deep.equal({  });
+         done();
+      })
+    })
+})
